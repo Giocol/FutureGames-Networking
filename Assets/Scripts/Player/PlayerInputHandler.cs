ad4fb156cd1338a5ebdb43c5227beeb4939a59a0
Jo Colomban <34074840+Giocol@users.noreply.github.com>
2024-08-12 18:34:57 +0200

    feat: setup basic input reading

---
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player
+{
+    public class PlayerInputHandler : MonoBehaviour
+    {
+        private InputAction moveAction;
+        private PlayerControls controls;
+
+        private void Awake()
+        {
+            controls = new PlayerControls();
+            moveAction = controls.MainPlayer.Move;
+        }
+
+        private void Update()
+        {
+            ReadMoveInput(moveAction.ReadValue<Vector2>());
+        }
+
+        private void ReadMoveInput(Vector2 direction)
+        {
+            //handle movement on player
+        }
+    }
+}

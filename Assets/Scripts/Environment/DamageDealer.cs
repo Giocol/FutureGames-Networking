6f2a89909591bb866dee46c5d123fb0f51eb6dca
Jo Colomban <34074840+Giocol@users.noreply.github.com>
2024-08-21 21:43:56 +0200

    feat: obstacles now despawn after a short time

--- Assets/Scripts/Environment/DamageDealer.cs
+++ Assets/Scripts/Environment/DamageDealer.cs
@@ -5,6 +5,21 @@ namespace Environment
 {
     public class DamageDealer : MonoBehaviour
     {
+        [SerializeField] private float timeToLive = 10;
+        private float timeAlive;
+
+        private void Start()
+        {
+            timeAlive = 0;
+        }
+
+        private void Update()
+        {
+            timeAlive += Time.deltaTime;
+            if(timeAlive >= timeToLive)
+                Destroy(gameObject);
+        }
+
         private void OnCollisionEnter2D (Collision2D other)
         {
             Player.Player player = other.gameObject.GetComponent<Player.Player>();
